80f931b6f4b9e83da62b299582813c4bf6064e41
ujjawal <supermandhani@gmail.com>
2021-02-17 16:15:07 +0530

    update2.0

---
+++ tree/binarytreetraversal/stackes.h
@@ -0,0 +1,52 @@
+/*struct node
+{
+	int data;
+	struct node * lchild;
+	struct node * rchild;
+}* root=NULL;
+*/
+struct stack
+{
+	struct node ** Q;
+	int top;
+	int size;
+};
+
+void createstack(struct stack * stk,int size)
+{
+	stk->size=size;
+	stk->top=-1;
+	stk->Q=(struct node **)malloc(sizeof(struct node *)*size);
+}
+
+void push(struct stack * stk,struct node * x)
+{
+	if(stk->top==stk->size)
+	{
+		printf("\nSeems that stack is already full\n");
+	}
+	else
+	{
+		stk->top++;
+		stk->Q[stk->top]=x;
+	}
+}
+
+struct node * pop(struct stack * stk)
+{
+	struct node * x=NULL;
+	if(stk->top==-1)
+		printf("\nSeems that your stack is already empty\n");
+	else
+	{
+		x=stk->Q[stk->top];
+		stk->top--;
+	}
+}
+int sisEmpty(struct stack * stk)
+{
+	if(stk->top==-1)
+		return 1;		/*return stk->top==-1;*/							
+	else
+		return 0;
+}
--- tree/binarytreetraversal/treetraversal.c
+++ tree/binarytreetraversal/treetraversal.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+#include"stackes.h"
 struct node
 {
 	int data;
@@ -16,32 +16,33 @@ struct queue
 	struct node ** Q;
 };
 
+
 void create(struct queue * q,int size)
 {
 	q->size=size;
 	q->front=q->rear=0;
-	q->Q=(struct node **)malloc(struct node *);
+	q->Q=(struct node **)malloc(sizeof(struct node *));
 }
 
-void enqueue(struct queue * q,node * x)
+void enqueue(struct queue * q,struct node * x)
 {
 	if((q->rear + 1)%q->size==q->front)
 		printf("\nQueue is full\n");
 	else
 	{
-		q->rear=(q->rear)%q->size;
+		q->rear=(q->rear + 1)%q->size;
 		q->Q[q->rear]=x;
 	}
 }
 
-node * dequeue(struct node * q)
+struct node * dequeue(struct queue * q)
 {
-	node * x=NULL;
+	struct node * x=NULL;
 	if(q->front==q->rear)
-		printf("\nQueue is full\n");
+		printf("\nQueue is empty\n");
 	else
 	{
-		q->front=(q->front + 1)q->size;
+		q->front=(q->front + 1)%q->size;
 		x=q->Q[q->front];
 	}
 	return x;
@@ -49,7 +50,7 @@ node * dequeue(struct node * q)
 
 int isEmpty(struct queue q)
 {
-	return q.front=q.rear;
+	return q.front==q.rear;
 }
 
 void Treecreate()
@@ -59,14 +60,16 @@ void Treecreate()
 	struct queue q;
 	create(&q,100);
 	printf("\nEnter the root value\n");
+	scanf("%d",&x);
 	root=(struct node *)malloc(sizeof(struct node));
 	root->data=x;
 	root->lchild=root->rchild=NULL;
 	enqueue(&q,root);
-	while(!isEmpty(&q))
+	while(!isEmpty(q))
 	{
 		p=dequeue(&q);
 		printf("\nEnter left child of %d\n",p->data);
+		printf("\nEnter -1 to stop grow the tree\n");
 		scanf("%d",&x);
 		if(x!=-1)
 		{
@@ -77,6 +80,7 @@ void Treecreate()
 			enqueue(&q,t);
 		}
 		printf("\nEnter the right child of %d\n",p->data);
+		printf("\nEnter -1 to stop grow the tree\n");
 		scanf("%d",&x);
 		if(x!=-1)
 		{
@@ -93,15 +97,36 @@ void preorder(struct node * p)
 {
 	if(p)
 	{
-		printf("%d",p->data);
+		printf("%d\n",p->data);
 		preorder(p->lchild);
 		preorder(p->rchild);
 	}
 }
 
+void inorder(struct node * p)
+{
+	if(p)
+	{
+		inorder(p->lchild);
+		printf("%d\n",p->data);
+		inorder(p->rchild);
+	}
+}
+
+void postorder(struct node * p)
+{
+	if(p)
+	{
+		postorder(p->lchild);
+		postorder(p->rchild);
+		printf("%d\n",p->data);
+	}
+}
 int main()
 {
 	Treecreate();
 	preorder(root);
+	inorder(root);
+	postorder(root);
 	return 0;
 }
